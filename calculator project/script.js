cc7671001d9f7a0cb2cd5ae3598fe398e6f844ed
Sudhanshu <sudhanshuchaubey273@gmail.com>
2022-05-25 13:37:26 +0530

    alpha test

--- calculator project/script.js
+++ calculator project/script.js
@@ -1,19 +1,20 @@
 var expression = "";
+var equation = 0;
 var searchbar = document.getElementById('search');
 var equals = document.getElementById('equal');
-equals.addEventListener('click', function(){
-    expression = document.getElementById('search').value;
-    console.log(eval(expression));
-    searchbar.value = eval(expression);
-    
-})
+// equals.addEventListener('click', function () {
+//     expression = document.getElementById('search').value;
+//     console.log(eval(expression));
+//     searchbar.value = eval(expression);
+// })
 
 
 // -------> AC Button
 
 var ac = document.getElementById('allClear');
-ac.addEventListener('click', function(){
+ac.addEventListener('click', function () {
     searchbar.value = "";
+    expression = "";
 })
 
 // -------->
@@ -23,7 +24,7 @@ ac.addEventListener('click', function(){
 
 var clear = document.getElementById('clear');
 
-clear.addEventListener('click', function(){
+clear.addEventListener('click', function () {
     expression = searchbar.value;
     searchbar.value = expression.slice(0, -1);
     expression = searchbar.value;
@@ -41,44 +42,127 @@ var seven = document.getElementById('seven');
 var eight = document.getElementById('eight');
 var nine = document.getElementById('nine');
 var zero = document.getElementById('zero');
+var dot = document.getElementById('dot');
 
-one.addEventListener('click', function(){
+
+one.addEventListener('click', function () {
     expression += "1";
     searchbar.value = expression;
 })
-two.addEventListener('click', function(){
+two.addEventListener('click', function () {
     expression += "2";
     searchbar.value = expression;
 })
-three.addEventListener('click', function(){
+three.addEventListener('click', function () {
     expression += "3";
     searchbar.value = expression;
 })
-four.addEventListener('click', function(){
+four.addEventListener('click', function () {
     expression += "4";
     searchbar.value = expression;
 })
-five.addEventListener('click', function(){
+five.addEventListener('click', function () {
     expression += "5";
     searchbar.value = expression;
 })
-six.addEventListener('click', function(){
+six.addEventListener('click', function () {
     expression += "6";
     searchbar.value = expression;
 })
-seven.addEventListener('click', function(){
+seven.addEventListener('click', function () {
     expression += "7";
     searchbar.value = expression;
 })
-eight.addEventListener('click', function(){
+eight.addEventListener('click', function () {
     expression += "8";
     searchbar.value = expression;
 })
-nine.addEventListener('click', function(){
+nine.addEventListener('click', function () {
     expression += "9";
     searchbar.value = expression;
 })
-zer0.addEventListener('click', function(){
+zero.addEventListener('click', function () {
     expression += "0";
     searchbar.value = expression;
 })
+dot.addEventListener('click', function () {
+        expression += ".";
+        searchbar.value = expression;
+})
+
+window.addEventListener('keydown', function (event) {
+    if ((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105)) {
+        expression += event.key;
+        searchbar.value = expression;
+    }
+    else if(event.key == "+" || event.key == "-" || event.key == "*" || event.key == "/" ||  event.key == ".")
+    {
+        searchbar.value = eval(expression);
+        expression = eval(expression);
+        expression += event.key;
+        searchbar.value = expression;
+    }
+    else if (event.keyCode === 8) {
+
+        expression = searchbar.value;
+        searchbar.value = expression.slice(0, -1);
+        expression = searchbar.value;
+    }
+    else if(event.keyCode == 13)
+    {
+        expression = eval(expression);
+        searchbar.value = expression;
+    }
+})
+
+//  ----->  Operations
+
+var add = document.getElementById('plus');
+var sub = document.getElementById('minus');
+var mul = document.getElementById('multiply');
+var div = document.getElementById('divide');
+var per = document.getElementById('percent');
+
+
+add.addEventListener('click', function(){
+    searchbar.value = eval(expression);
+    expression = eval(expression);
+    expression += "+";
+    searchbar.value = expression;
+})
+sub.addEventListener('click', function(){
+    searchbar.value = eval(expression);
+    expression = eval(expression);
+    expression += "-";
+    searchbar.value = expression;
+})
+mul.addEventListener('click', function(){
+    searchbar.value = eval(expression);
+    expression = eval(expression);
+    expression += "*";
+    searchbar.value = expression;
+})
+div.addEventListener('click', function(){
+    searchbar.value = eval(expression);
+    expression = eval(expression);
+    expression += "/";
+    searchbar.value = expression;
+})
+per.addEventListener('click', function(){
+    searchbar.value = eval(expression);
+    expression = eval(expression);
+    expression = expression - 0;
+    expression /= 100;
+    searchbar.value = expression;
+})
+
+
+
+equals.addEventListener('click', function(){
+    expression = eval(expression);
+    searchbar.value = expression;
+})
+
+
+
+//  ------>
